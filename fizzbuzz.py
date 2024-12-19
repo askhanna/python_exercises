1e0d82d9d7a180d9f840083dcfc748910971ee89
Ashish Khanna <askhanna314@gmail.com>
2024-12-19 07:47:08 +0100

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz(upTo):
+    # Loop from 1 up to (and including) the upTo parameter:
+    for num in range(1, upTo + 1):
+        if (num % 3 == 0) and (num % 5 == 0):
+            print("FizzBuzz", end=' ') 
+        elif (num % 3 == 0):
+            print("Fizz", end=' ')
+        elif (num % 5 == 0):
+            print("Buzz", end=' ')
+        else:
+            print(num, end=' ')
+
+if __name__ == "__main__":
+    upTo = 10
+    fizzbuzz(upTo)
